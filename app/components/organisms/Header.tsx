1ed7816bf800f5afafec5f2707e0959f37f24c53
Alexis Guzman <alexis12989x@gmail.com>
2022-09-28 15:59:35 -0500

    feat(organisms): setup header component

--- app/components/organisms/Header.tsx
+++ app/components/organisms/Header.tsx
@@ -1,8 +1,14 @@
+import { Link } from '@remix-run/react';
+
+import { Nav } from '~/components//molecules';
+
 const Header = () => {
   return (
-    <div>
-      <p>Header Component</p>
-    </div>
+    <header className='flex justify-between'>
+      <Link to='/'>codingcodax</Link>
+
+      <Nav />
+    </header>
   );
 };
 
