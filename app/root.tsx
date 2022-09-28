8d9fb7135c9db51c2b43300fb18d24edb40c6437
Alexis Guzman <alexis12989x@gmail.com>
2022-09-28 10:45:03 -0500

    feat(root): import tailwindcss styles and create links function

--- app/root.tsx
+++ app/root.tsx
@@ -8,11 +8,17 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 
+import styles from '~/styles/app.css';
+
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1',
 });
+
+export const links = () => {
+  return [{ rel: 'stylesheet', href: styles }];
+};
 
 const App = () => {
   return (
