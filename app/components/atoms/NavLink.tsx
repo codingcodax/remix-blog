9797a8fec74ad149a492eeafa024600fb893f84a
Alexis Guzman <alexis12989x@gmail.com>
2022-09-28 14:02:27 -0500

    feat(atoms): setup nav link component

--- app/components/atoms/NavLink.tsx
+++ app/components/atoms/NavLink.tsx
@@ -1,8 +1,16 @@
-const NavLink = () => {
+import type { FC, ReactNode } from 'react';
+import { Link } from '@remix-run/react';
+
+interface NavLinkProps {
+  href: string;
+  children: ReactNode;
+}
+
+const NavLink: FC<NavLinkProps> = ({ href, children }) => {
   return (
-    <div>
-      <p>Nav Link Component</p>
-    </div>
+    <li className='duration-300 hover:text-indigo-700'>
+      <Link to={href}>{children}</Link>
+    </li>
   );
 };
 
