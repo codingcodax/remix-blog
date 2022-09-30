efb1bef984d3027845bff52d9bf66164c33f07bd
Alexis Guzman <alexis12989x@gmail.com>
2022-09-30 18:00:02 -0500

    feat(routes): setup post id component

--- app/routes/posts/$postId.tsx
+++ app/routes/posts/$postId.tsx
@@ -1,8 +1,22 @@
+import type { LoaderFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+import { db } from '~/services/db';
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const post = await db.post.findUnique({ where: { id: params.postId } });
+
+  return { post };
+};
+
 const Post = () => {
+  const { post } = useLoaderData();
+
   return (
-    <div>
-      <p>Post Component</p>
-    </div>
+    <article className='prose'>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </article>
   );
 };
 
