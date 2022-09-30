50f7d504c54892fdc4f4d13b33b5d979fd8a2135
Alexis Guzman <alexis12989x@gmail.com>
2022-09-30 13:11:35 -0500

    feat(routes): create post component

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,27 @@
+import { useLoaderData } from '@remix-run/react';
+import type { Post } from '@prisma/client';
+
+import { db } from '~/services/db';
+
+export const loader = async () => {
+  const posts = await db.post.findMany();
+
+  return { posts };
+};
+
 const Posts = () => {
+  const { posts } = useLoaderData<{ posts: Post[] }>();
+
   return (
     <div>
       <p>Post Component</p>
+
+      {posts.map(({ id, title, body }) => (
+        <div key={id}>
+          <h2>{title}</h2>
+          <p>{body}</p>
+        </div>
+      ))}
     </div>
   );
 };
