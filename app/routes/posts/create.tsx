a177912070a32ddfe2d3f919e9e83e3527186b86
Alexis Guzman <alexis12989x@gmail.com>
2022-09-30 13:11:03 -0500

    feat(routes): setup post create component

--- app/routes/posts/create.tsx
+++ app/routes/posts/create.tsx
@@ -1,7 +1,35 @@
+import type { ActionFunction } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+import { Form } from '@remix-run/react';
+
+import { db } from '~/services/db';
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const title = form.get('title');
+  const body = form.get('body');
+
+  const data = { title, body };
+  // @ts-ignore
+  const post = await db.post.create({ data });
+
+  return redirect(`/posts/${post.id}`);
+};
+
 const Create = () => {
   return (
     <div>
-      <p>Create Component</p>
+      <h2>Create new post</h2>
+
+      <Form method='post'>
+        <label htmlFor='title'>Title</label>
+        <input type='text' name='title' id='title' />
+
+        <label htmlFor='body'>Body</label>
+        <textarea name='body' id='body' />
+
+        <button type='submit'>Add new post</button>
+      </Form>
     </div>
   );
 };
