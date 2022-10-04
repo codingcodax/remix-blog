302293e83b416e820c65efc4f7d376ce49f2682a
Alexis Guzman <alexis12989x@gmail.com>
2022-10-04 14:24:17 -0500

    feat(routes): setup home and about pages and export meta function for titles

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,7 +1,24 @@
+import type { MetaFunction } from '@remix-run/node';
+
+export const meta: MetaFunction = () => ({
+  title: 'About | codingcodax',
+});
+
 const About = () => {
   return (
-    <div>
-      <p>About Component</p>
+    <div className='prose'>
+      <h1>About</h1>
+      <p>
+        This project was built by{' '}
+        <a
+          href='https://www.codingcodax.dev/'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          codingcodax
+        </a>
+        .
+      </p>
     </div>
   );
 };
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,26 @@
 const Index = () => {
   return (
-    <div>
-      <p>Hello World!</p>
+    <div className='prose'>
+      <h1>Remix Blox</h1>
+      <p>
+        This is a remix blog built with{' '}
+        <a href='https://remix.run/' rel='noopener noreferrer' target='_blank'>
+          remix
+        </a>
+        , and{' '}
+        <a
+          href='https://www.prisma.io/'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          prisma
+        </a>
+        .
+      </p>
+
+      <p>
+        It's recommended to test it locally, because it uses sqlite as database.
+      </p>
     </div>
   );
 };
