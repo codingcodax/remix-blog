51e9e931fd559038d2301a04800bbfe0759d8f08
Alexis Guzman <alexis12989x@gmail.com>
2022-10-04 13:47:52 -0500

    feat(routes): setup error boundary

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -10,6 +10,28 @@ export const loader: LoaderFunction = async () => {
   return { posts };
 };
 
+export const ErrorBoundary = () => {
+  return (
+    <div>
+      <h2 className='text-4xl font-bold'>OOPS</h2>
+      <p>Something went wrong</p>
+      <p className='mb-8'>
+        This error is caused by the sqlite database. This is an example.
+      </p>
+
+      <div className='p-4 rounded-md bg-base-200'>
+        <Link
+          to='/posts/b0efd03a-9819-45d5-adcb-99e2d9fd38ec'
+          className='text-xl font-bold duration-200 hover:text-primary'
+        >
+          Mi primer post en remix
+        </Link>
+        <p>Esto es el contenido</p>
+      </div>
+    </div>
+  );
+};
+
 const Posts = () => {
   const { posts } = useLoaderData<{ posts: Post[] }>();
 
