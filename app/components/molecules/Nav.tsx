44be3ed69fd24afc99a0d58f1148e7168759244e
Alexis Guzman <alexis12989x@gmail.com>
2022-10-04 14:03:43 -0500

    feat(molecules): add reponsive dropdown nav

--- app/components/molecules/Nav.tsx
+++ app/components/molecules/Nav.tsx
@@ -5,7 +5,7 @@ import { NavLink } from '~/components/atoms';
 const Nav = () => {
   return (
     <nav className='flex-none'>
-      <ul className='menu menu-horizontal p-0 grid grid-flow-col gap-4'>
+      <ul className='hidden menu menu-horizontal p-0 sm:grid grid-flow-col gap-4'>
         <NavLink to='/'>Home</NavLink>
         <li tabIndex={0}>
           <RemixNavLink to='/posts'>
@@ -26,6 +26,34 @@ const Nav = () => {
         </li>
         <NavLink to='/about'>About</NavLink>
       </ul>
+
+      <div className='dropdown sm:hidden navbar-start'>
+        <label tabIndex={0} className='btn btn-ghost'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-5 w-5'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M4 6h16M4 12h16M4 18h7'
+            />
+          </svg>
+        </label>
+        <ul
+          tabIndex={0}
+          className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
+        >
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/posts'>Posts</NavLink>
+          <NavLink to='/posts/create'>Create Post</NavLink>
+          <NavLink to='/about'>About</NavLink>
+        </ul>
+      </div>
     </nav>
   );
 };
